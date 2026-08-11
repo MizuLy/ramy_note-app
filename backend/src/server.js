5b761ad3c3003bc0ec7002ebd5bd13b2b649cfc8
Sengly Sea <optionalsa@gmail.com>
2026-08-11 10:51:38 +0700

    add: render url to cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 6969;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ramy-note-app.vercel.app"],
     credentials: true,
   }),
 );
