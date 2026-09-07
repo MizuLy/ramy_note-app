36aad29faca0400404964022c0d64a3af28e7e88
Sengly Sea <optionalsa@gmail.com>
2026-09-07 09:49:11 +0700

    generate pwa asset

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,11 +10,19 @@ export default defineConfig({
       manifest: {
         name: "Ramy",
         short_name: "Ramy",
-        description: "Your app description here",
+        description: "A note-taking app",
         theme_color: "#ffffff",
         icons: [
-          { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
+          {
+            src: "/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
         ],
       },
     }),
